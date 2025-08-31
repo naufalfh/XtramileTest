d9c5236510d1b6fceb4485b61217686f49b78b24
naufalfh <naufalrog@gmail.com>
2025-08-31 13:17:02 +0700

    first commit

---
+++ tests/pages/elements.js
@@ -0,0 +1,16 @@
+export const elements = {
+    // login
+    "usernameField": "#user-name",
+    "passwordField": "#password",
+    "loginButton": "#login-button",
+    "errorMessage": "[data-test=\"error\"]",
+    "productTitle": ".title",
+    // invalidLogin
+    "invalidIcon1": "//div[@class='form_group'][1]//*[@data-icon='times-circle']",
+    "invalidIcon2": "//div[@class='form_group'][2]//*[@data-icon='times-circle']",
+    "errorMessageLogin": "//h3[@data-test='error']",
+    // product
+    "inventoryContainer": "//div[@data-test='inventory-container']",
+    "sortButton": "//select[@data-test='product-sort-container']",
+    "firstItem": "//div[@data-test='inventory-item'][1]"
+}
---
+++ tests/pages/sauceDemoObj.js
@@ -0,0 +1,42 @@
+import { expect } from "@playwright/test";
+
+class SauceDemoPage {
+    constructor(page) {
+        this.page = page;
+        // this.usernameField = page.locator('#user-name');
+        // this.passwordField = page.locator('#password');
+        // this.loginButton = page.locator('#login-button');
+        // this.errorMessage = page.locator('[data-test="error"]');
+        // this.productTitle = page.locator('.title');
+    }
+
+    async navigateToUrl(url) {
+        await this.page.goto(url);
+    }
+
+    async clickElement(element) {
+        await this.page.locator(element).click();
+    }
+
+    async inputValue(element, value) {
+        await this.page.locator(element).fill(value);
+    }
+
+    async verifyElementIsVisible(element) {
+        await expect(this.page.locator(element)).toBeVisible();
+    }
+
+    async verifyTextIsVisible(text) {
+        await expect(this.page.getByText(text)).toBeVisible();
+    }
+
+    async verifyElementContainsText(element, text) {
+        await expect(this.page.locator(element)).toContainText(text);
+    }
+
+    async verifyUrlContains(urlPath) {
+        await expect(this.page).toHaveURL(new RegExp(urlPath));
+    }
+}
+
+export { SauceDemoPage };
